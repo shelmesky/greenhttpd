e83451bbc90745ae0e8f86ab5b419fb0f307703c
bodogit <qiaoliang0222@163.com>
2013-11-11 16:25:37 +0800

    Fixed compared error for linux
    Issues:
    In file included from lib/util-internal.h:41,
                     from http.c:40:
    lib/ipv6-internal.h:51: error: redefinition of 鈥榮truct in6_addr鈥?
    lib/ipv6-internal.h:52: error: expected 鈥?鈥? 鈥?鈥? 鈥?鈥? 鈥榼鈥?or 鈥榑_attribute__鈥?before 鈥?鈥?token
    lib/ipv6-internal.h:57: error: conflicting types for 鈥榮a_family_t鈥?
    /usr/include/bits/sockaddr.h:29: note: previous declaration of 鈥榮a_family_t鈥?was here
    lib/ipv6-internal.h:61: error: redefinition of 鈥榮truct sockaddr_in6鈥?
    In file included from http.c:40:
    lib/util-internal.h:219: error: redefinition of 鈥榮truct sockaddr_storage鈥?
    gcc: http.o: No such file or directory

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cc -c servlet.c
-cc -c http.c
+cc -c http.c -D_EVENT_HAVE_STRUCT_IN6_ADDR -D_EVENT_HAVE_SA_FAMILY_T -D_EVENT_HAVE_STRUCT_SOCKADDR_IN6 -D_EVENT_H -D_EVENT_HAVE_STRUCT_SOCKADDR_STORAGE:
 gcc http.o servlet.o -o httpd -levent
